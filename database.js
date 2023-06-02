b4807d936d8d4935a31ed23bc3da64ca3b7a0f4b
Vansh <vanshk605@gmail.com>
2023-06-02 05:47:21 +0530

    helo

--- database.js
+++ database.js
@@ -4,8 +4,10 @@ const mongoURI = 'mongodb+srv://vansh:vansh@cluster0.aubleqa.mongodb.net/'
 
 
 const connectToMongo =async ()=>{
-    mongoose.connect(mongoURI).then(()=>{
+    mongoose.connect(mongoURI).then(()  =>{
         console.log("connected");
+    }).catch((E)=>{
+        console.log(E);
     })
 }
 
